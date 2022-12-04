7acaeea2aadfb96be7f91f79e7e294131f9571fd
Erich Eichinger <eeichinger@gmail.com>
2022-12-04 20:40:39 +0100

    complete day 03, part 2

--- 2022/03/03.py
+++ 2022/03/03.py
@@ -6,17 +6,16 @@ ITEM_PRIORITY = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def split_rucksack_compartments(rucksack) -> List[Set]:
+    N = 2  # number of evenly sized compartments
     rucksack_size = len(rucksack)
-    comp_size = rucksack_size//2
-    comp1_set = set(rucksack[:comp_size])
-    comp2_set = set(rucksack[comp_size:])
-    return [comp1_set, comp2_set]
+    comp_size = rucksack_size // N
+    compartments = [rucksack[i:i + comp_size] for i in range(0, len(rucksack), comp_size)]
+    return compartments
 
 
-def find_shared_itemtype(compartments: List[Set]) -> str:
-    [comp1set, comp2set] = compartments
-    common_el = (comp1set & comp2set).pop()
-    return common_el
+def find_shared_itemtype(compartments: List[str]) -> str:
+    sets = [set(compartment) for compartment in compartments]
+    return set.intersection(*sets).pop()
 
 
 def determine_itemtype_prio(item: str) -> int:
@@ -24,24 +23,38 @@ def determine_itemtype_prio(item: str) -> int:
     return prio
 
 
-def determine_rucksack_prio(line:str) -> int:
-    compartments = split_rucksack_compartments(line.strip())
+def determine_rucksack_prio(rucksack: str) -> int:
+    compartments = split_rucksack_compartments(rucksack)
     itemtype = find_shared_itemtype(compartments)
     prio = determine_itemtype_prio(itemtype)
-    # print("itemtype:", itemtype, ", prio:", prio)
     return prio
 
 
-def calculate_prios_sum(input: List[str]) -> int:
-    prios = [determine_rucksack_prio(line) for line in input]
-    return sum(prios)
+def calculate_prio_total(rucksacks: List[str]) -> int:
+    prios = [determine_rucksack_prio(rucksack) for rucksack in rucksacks]
+    prios_total = sum(prios)
+    return prios_total
 
 
-with open('03.txt') as f:
-    total_prio = calculate_prios_sum(f)
+def calculate_badge_prio_total(rucksacks) -> int:
+    N = 3  # group size
+    grouped_rucksacks = [rucksacks[i:i + N] for i in range(0, len(rucksacks), N)]
+    badges = [find_shared_itemtype(group) for group in grouped_rucksacks]
+    badge_prios = [determine_itemtype_prio(badge) for badge in badges]
+    total_badge_prios = sum(badge_prios)
+    return total_badge_prios
+
+
+def main(fname: str):
+    with open(fname) as f:
+        rucksacks: List[str] = [line.strip() for line in f]
+
+    total_prio = calculate_prio_total(rucksacks)
     print("total_prio:", total_prio)
 
-# print(ord('A'))
-# print(ord('a'))
-# print(chr(ord('A')-1))
-# print(chr(ord('a')-1))
+    total_badge_prio = calculate_badge_prio_total(rucksacks)
+    print("total_badge_prio:", total_badge_prio)
+
+
+if __name__ == '__main__':
+    main('03.txt')
