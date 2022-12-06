c3bbd694e4993105bdd4496eac054181016af4e9
Erich Eichinger <eeichinger@gmail.com>
2022-12-06 09:16:42 +0100

    complete day 06

---
+++ 2022/06/06.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from typing import List, Set, Dict
+
+
+def detect_message(input: str, marker_size: int, expected_result: int):
+    result = 0
+    buffer = []
+    marker = ['not processed yet']
+    for ch in input:
+        buffer.append(ch)
+        marker = buffer[-marker_size:]
+        if len(set(marker)) == marker_size:
+            result = len(buffer)
+            break
+
+    print("result: {}, expected_result:{}, marker:{}, buffer:{}"
+          .format(result, expected_result, "".join(marker), "".join(buffer)))
+    assert result == expected_result
+
+
+def main(source: List):
+    for line in source:
+        [input, marker_size, expected_result] = line.split(':')
+        detect_message(input, int(marker_size), int(expected_result))
+
+
+if __name__ == '__main__':
+    with open('06-test2.txt') as f:
+        main(f)
