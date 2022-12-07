e8865d9b53ba824fa63818cbf62654403df925f0
Erich Eichinger <eeichinger@gmail.com>
2022-12-07 18:54:19 +0100

    complete day 07, even shorter non-recursive solution

--- 2022/07/notree.py
+++ 2022/07/notree.py
@@ -1,66 +1,43 @@
 #!/usr/bin/env python3
 
-from typing import List, Set, Dict
+from typing import List, Set, Dict, Iterable
 import re
-
-
-def is_int(val):
-    return val.lstrip("-+").isdigit()
 
 
 def cd(curdir: List[str], targetdir) -> List[str]:
     if targetdir == '/':
-        curdir = []
+        return []
     elif targetdir == '..':
-        curdir.pop()
-    else:
-        curdir = curdir + [targetdir]
-    return curdir
+        return curdir[:-1]
+    return curdir + [targetdir]
 
 
-def to_dirpath(curdir: List[str]) -> str:
+def update_total_size_recursive(total_dir_sizes: dict[str, int], curdir: List[str], size: int):
     dirpath = "/" + "/".join(curdir)
-    if len(dirpath) > 1:
-        dirpath += "/"
-    return dirpath
+    if dirpath not in total_dir_sizes:
+        total_dir_sizes[dirpath] = 0
+    total_dir_sizes[dirpath] += size
+    if curdir:
+        # update parent size if not already root
+        update_total_size_recursive(total_dir_sizes, curdir[:-1], size)
 
 
-def to_filepath(curdir: List[str], filename) -> str:
-    filepath = curdir + [filename]
-    return "/" + "/".join(filepath)
-
-
-def main(source: List[str]) -> None:
-    curdir = []  # current directory parts list, e.g. '/a/e' as [ 'a', 'e' ]
-    filesizes = dict()  # dict with key=fq filepath, val=size
-    dirset = set()  # set of unique fq dir names
-
+def main(source: Iterable[str], expected_result: str) -> None:
     # customised: first line contains expected size for auto-verification:
     #   expected_size_part1:expected_size_part2
-    (expected_size1, expected_size2) = [int(sise) for sise in source[0].split(":")]
+    (expected_size1, expected_size2) = [int(size) for size in expected_result.split(":")]
 
-    for line in source[1:]:
+    curdir = []  # current directory parts list, e.g. '/a/e' as [ 'a', 'e' ]
+    total_dir_sizes = dict()
+
+    for line in source:
         argv = line.strip().split(' ')
         if argv[0] == '$' and argv[1] == 'cd':
             # found cd instr, calc new curdir
             curdir = cd(curdir, argv[2])
-            dirpath = to_dirpath(curdir)  # convert path fragments to path string with begin&trailing slash
-            dirset.add(dirpath)  # record dir name (set auto-dedups names)
-        elif is_int(argv[0]):
-            # found file, record filesize with full path
-            filepath = to_filepath(curdir, argv[1])
-            filesizes[filepath] = int(argv[0])
-    # print(dirset)
-    # print(filesizes)
-    total_dir_sizes = dict()
-    # calculate total size for each dir
-    for adir in dirset:
-        # find all files under adir
-        dirfilesizes = [val for key, val in filesizes.items() if key.startswith(adir)]
-        total_dir_size = sum(dirfilesizes)
-        # print("dir: {}, size:{}, files:{}".format(adir, total_dir_size, dirfilesizes))
-        total_dir_sizes[adir] = total_dir_size
-    # print(total_dir_sizes)
+        elif argv[0].isdigit():
+            # found file, add filesize in all dirs up the hierarchy
+            update_total_size_recursive(total_dir_sizes, curdir, int(argv[0]))
 
     # part 1
     nodes_smaller_than_100000 = [adirsize for adirsize in total_dir_sizes.values() if adirsize <= 100000]
@@ -80,6 +57,8 @@ def main(source: List[str]) -> None:
 
 if __name__ == '__main__':
     with open('07-test.txt') as f:
-        main(f.readlines())
+        expected = f.readline()
+        main(f, expected)
     with open('07-data.txt') as f:
-        main(f.readlines())
+        expected = f.readline()
+        main(f, expected)
