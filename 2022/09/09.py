e9e44a86b10a9bcf58ec2233cdaddf613924bf82
Erich Eichinger <eeichinger@gmail.com>
2022-12-09 16:31:23 +0100

    complete day 09, part1 refactored for part2

--- 2022/09/09.py
+++ 2022/09/09.py
@@ -1,68 +1,63 @@
 #!/usr/bin/env python3
 
-from typing import List, Set, Dict, Iterable
-import re
+from typing import List, Set, Iterable
 
 
-def move_step_right(posH: tuple[int, int], posT: tuple[int, int], visitedT: Set[tuple[int, int]]):
-    return move_step(posH, (posH[0] + 1, posH[1]), posT, visitedT)
+def move(knot: tuple[int, int], x: int, y: int):
+    return (knot[0] + x, knot[1] + y)
 
 
-def move_step_left(posH: tuple[int, int], posT: tuple[int, int], visitedT: Set[tuple[int, int]]):
-    return move_step(posH, (posH[0] - 1, posH[1]), posT, visitedT)
+def move_rope_head_single_step(rope: List[tuple[int, int]], x, y) -> List[tuple[int, int]]:
+    for i in range(0, len(rope) - 1):
+        (new_posH, new_posT) = move_single_knot(rope[i], move(rope[i], x, y), rope[i + 1])
+        rope[i] = new_posH
+        rope[i + 1] = new_posT
+    return rope
 
 
-def move_step_up(posH: tuple[int, int], posT: tuple[int, int], visitedT: Set[tuple[int, int]]):
-    return move_step(posH, (posH[0], posH[1] + 1), posT, visitedT)
-
-
-def move_step_down(posH: tuple[int, int], posT: tuple[int, int], visitedT: Set[tuple[int, int]]):
-    return move_step(posH, (posH[0], posH[1] - 1), posT, visitedT)
-
-
-def move_step(old_posH: tuple[int, int], new_posH: tuple[int, int], posT: tuple[int, int],
-              visitedT: Set[tuple[int, int]]):
-    if not(abs(new_posH[0] - posT[0]) <= 1 and abs(new_posH[1] - posT[1]) <= 1):
+def move_single_knot(old_posH: tuple[int, int], new_posH: tuple[int, int], posT: tuple[int, int]):
+    if not (abs(new_posH[0] - posT[0]) <= 1 and abs(new_posH[1] - posT[1]) <= 1):
         posT = old_posH
-    return (new_posH, posT, visitedT)
+    # print("posH[{}], posT[{}]".format(new_posH, posT))
+    return (new_posH, posT)
 
 
-def processInstruction(instr: tuple[str, int], posH: tuple[int, int], posT: tuple[int, int],
-                       visitedT: Set[tuple[int, int]]) -> tuple[tuple[int, int], tuple[int, int], set[tuple[int, int]]]:
+def processInstruction(instr: tuple[str, int], rope: List[tuple[int, int]], visitedT: Set[tuple[int, int]]) \
+    -> tuple[List[tuple[int, int]], set[tuple[int, int]]]:
     (direction, steps) = instr
+    # print("processing dir:{}, steps:{}".format(direction, steps))
     if direction.lower() == "r":
         for step in range(0, steps):
-            (posH, posT, visitedT) = move_step_right(posH, posT, visitedT)
-            visitedT.add(posT)
+            rope = move_rope_head_single_step(rope, 1, 0)
+            visitedT.add(rope[-1])
     elif direction.lower() == "l":
         for step in range(0, steps):
-            (posH, posT, visitedT) = move_step_left(posH, posT, visitedT)
-            visitedT.add(posT)
+            rope = move_rope_head_single_step(rope, -1, 0)
+            visitedT.add(rope[-1])
     elif direction.lower() == "u":
         for step in range(0, steps):
-            (posH, posT, visitedT) = move_step_up(posH, posT, visitedT)
-            visitedT.add(posT)
+            rope = move_rope_head_single_step(rope, 0, 1)
+            visitedT.add(rope[-1])
     elif direction.lower() == "d":
         for step in range(0, steps):
-            (posH, posT, visitedT) = move_step_down(posH, posT, visitedT)
-            visitedT.add(posT)
+            rope = move_rope_head_single_step(rope, 0, -1)
+            visitedT.add(rope[-1])
     else:
         print("cant process {}".format(instr))
         assert False
-    return (posH, posT, visitedT)
+    return (rope, visitedT)
 
 
 def main(source: Iterable[str], expected_result: str) -> None:
     (expected_result1, expected_result2) = [int(res) for res in expected_result.split(":")]
 
-    posH: tuple[int, int] = (0, 0)
-    posT: tuple[int, int] = (0, 0)
+    rope = [(0, 0) for i in range(0, 2)]
     visitedT: Set[tuple[int, int]] = set()
-    visitedT.add(posT)
+    visitedT.add(rope[-1])
     for line in source:
         (direction, stepsStr) = line.split(" ")
         instr = (direction, int(stepsStr))
-        (posH, posT, visitedT) = processInstruction(instr, posH, posT, visitedT)
+        (rope, visitedT) = processInstruction(instr, rope, visitedT)
 
     result1 = len(visitedT)
     print("visited: {}, expected: {}".format(result1, expected_result1))
@@ -71,27 +66,27 @@ def main(source: Iterable[str], expected_result: str) -> None:
 
 def test_same_position_move_step_right():
     # test: same position, move right
-    (newH, newT, visitedT) = move_step_right((0, 0), (0, 0), set())
-    assert newH == (1, 0)
-    assert newT == (0, 0)
-    assert (0, 0) in visitedT
+    rope1 = [(0, 0), (0, 0)]
+    rope = move_rope_head_single_step(rope1, 1, 0)
+    assert rope[0] == (1, 0)
+    assert rope[-1] == (0, 0)
 
 
 def test_oneoff_position_move_step_right():
-    (newH, newT, visitedT) = move_step_right((1, 0), (0, 0), set())
-    assert newH == (2, 0)
-    assert newT == (1, 0)
-    assert (1, 0) in visitedT
+    rope1 = [(1, 0), (0, 0)]
+    rope = move_rope_head_single_step(rope1, 1, 0)
+    assert rope[0] == (2, 0)
+    assert rope[-1] == (1, 0)
 
-    (newH, newT, visitedT) = move_step_right((1, 1), (0, 0), set())
-    assert newH == (2, 1)
-    assert newT == (1, 1)
-    assert (1, 1) in visitedT
+    rope2 = [(1, 1), (0, 0)]
+    rope = move_rope_head_single_step(rope2, 1, 0)
+    assert rope[0] == (2, 1)
+    assert rope[-1] == (1, 1)
 
-    (newH, newT, visitedT) = move_step_right((0, 0), (1, 0), set())
-    assert newH == (1, 0)
-    assert newT == (1, 0)
-    assert (1, 0) in visitedT
+    rope3 = [(0, 0), (1, 0)]
+    rope = move_rope_head_single_step(rope3, 1, 0)
+    assert rope[0] == (1, 0)
+    assert rope[-1] == (1, 0)
 
 
 if __name__ == '__main__':
