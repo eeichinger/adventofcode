512d621d1e73bc0163e558e03883576c0205b891
Erich Eichinger <eeichinger@gmail.com>
2022-12-12 22:04:23 +0100

    more classifying with @dataclass and @property

--- 2022/12/12.py
+++ 2022/12/12.py
@@ -6,36 +6,46 @@ from dataclasses import dataclass
 from collections import deque
 
 
+@dataclass(frozen=True, slots=True)
+class Point:
+    x: int
+    y: int
+
+    @staticmethod
+    def of_tuple(other: tuple[int, int]):
+        return Point(other[0], other[1])
+
+    @staticmethod
+    def of_xy(x: int, y: int):
+        return Point(x, y)
+
+    def add(self, other: 'Point'):
+        return Point(self.x + other.x, self.y + other.y)
+
+
 # A queue node used in BFS
-@dataclass(frozen=True)
+@dataclass(frozen=True, slots=True)
 class Node:
-    __slots__ = ['parent', '_coords']
+    _coords: Point
+    parent: 'Node' = None
 
-    # (x, y) represents coordinates of a cell in the matrix
-    # maintain a parent node for the printing path
-    def __init__(self, coords: tuple[int, int], parent=None):
-        object.__setattr__(self, "_coords", coords)
-        object.__setattr__(self, "parent", parent)
+    @property
+    def x(self) -> int:
+        return self._coords.x
 
-    def __repr__(self):
-        return str(self._coords)
+    @property
+    def y(self) -> int:
+        return self._coords.y
 
-    def __eq__(self, otherNode):
-        return self.is_match((other.x, other.y))
-
-    def get_x(self):
-        return self._coords[0]
-    x = property(fget=get_x, doc='x coordinate')
-
-    def get_y(self):
-        return self._coords[1]
-    y = property(fget=get_y, doc='x coordinate')
-
-    def key(self):
+    @property
+    def key(self) -> Point:
         return self._coords
 
-    def is_match(self, other: tuple[int, int]):
+    def is_match(self, other: Point):
         return self._coords == other
+
+    def add(self, other: Point):
+        return Node(self._coords.add(other))
 
 
 # Below lists detail all four possible movements from a cell
@@ -51,25 +61,21 @@ def getPath(node, path=()):
 
 
 # Find the shortest route in a matrix from source cell to destination cell
-def findPath(matrix, start=(0, 0), end=(0, 0)):
+def findPath(matrix, start_pos: Point = Point(0, 0), end_pos: Point = Point(0, 0)):
     # base case
     if not matrix or not len(matrix):
         return
 
     # matrix size for bounds-checking
-    N_x = len(matrix[0])
-    N_y = len(matrix)
+    N = Point(len(matrix[0]), len(matrix))
 
     # create a queue and enqueue the first node
     q: Deque[Node] = deque()
-    src = Node(start)
+    src = Node(start_pos)
     q.append(src)
 
     # set to check if the matrix cell is visited before or not
-    visited = set()
-
-    # next_key = (src.x, src.y)
-    visited.add(src.key())
+    visited_coords = {src.key}
 
     # loop till queue is empty
     while q:
@@ -81,7 +87,7 @@ def findPath(matrix, start=(0, 0), end=(0, 0)):
         # print("current node ({},{},{}), queuesize={}".format(curr.x, curr.y, curr_height, len(q)))
         # return if the destination is found
         # if curr.x == end[0] and curr.y == end[1]:
-        if curr.is_match(end):
+        if curr.is_match(end_pos):
             # print("REACHED END ({},{},{})".format(curr.x, curr.y, curr_height))
             path = []
             getPath(curr, path)
@@ -91,78 +97,72 @@ def findPath(matrix, start=(0, 0), end=(0, 0)):
         # and recur for each valid movement
         for allowed_movement in allowed_movements:
             # get next position coordinates using the value of the current cell
-            next_x = curr.x + allowed_movement[0]
-            next_y = curr.y + allowed_movement[1]
+            candidate_coords = Point(curr.x + allowed_movement[0], curr.y + allowed_movement[1])
 
             # bounds-checking
-            if not ((0 <= next_x < N_x) and (0 <= next_y < N_y)):
+            if not ((0 <= candidate_coords.x < N.x) and (0 <= candidate_coords.y < N.y)):
                 continue
             # check if it is possible to go to the next position
             # from the current position
-            next_height = matrix[next_y][next_x]
-            next_key = (next_x, next_y)
+            next_height = matrix[candidate_coords.y][candidate_coords.x]
             # print("    checking node ({},{},{})".format(next_x, next_y, next_height))
             if next_height - curr_height > 1:
                 continue
-            if (next_key in visited):
+            if (candidate_coords in visited_coords):
                 # print("    --> already seen")
                 continue
 
             # enqueue it and mark it as visited
             # print("    --> found candidate ({},{},{})".format(next_x, next_y, next_height))
-            q.append(Node((next_x, next_y), curr))
-            visited.add(next_key)
+            q.append(Node(candidate_coords, curr))
+            visited_coords.add(candidate_coords)
 
     # return None if the path is not possible
     return
 
 
-def parse_heightmap(source):
+def parse_heightmap(source: List[str]) -> tuple[List[List[int]], Point, Point]:
     matrix: List[List[int]] = []
-    start_x = -1
-    start_y = -1
-    end_x = -1
-    end_y = -1
+    start_pos: Point = Point(-1, -1)
+    end_pos: Point = Point(-1, -1)
     for line in source:
         if ('S' in line):
-            start_x = line.index('S')
-            start_y = len(matrix)
+            start_pos = Point(line.index('S'), len(matrix))
             line = line.replace('S', 'a')
         if ('E' in line):
-            end_x = line.index('E')
-            end_y = len(matrix)
+            end_pos = Point(line.index('E'), len(matrix))
             line = line.replace('E', 'z')
 
         heights_str = line.strip()
         print(heights_str)
         heights = [(ord(h) - ord('a')) for h in heights_str]
         matrix.append(heights)
-    return matrix, (start_x, start_y), (end_x, end_y)
+    return matrix, start_pos, end_pos
 
 
 def main(source: List[str], expected_result: str) -> None:
-    matrix, (start_x, start_y), (end_x, end_y) = parse_heightmap(source)
+    (matrix, start_pos, end_pos) = parse_heightmap(source)
 
-    print("found start at ({}, {})".format(start_x, start_y))
-    print("found end at ({}, {})".format(end_x, end_y))
-    all_start_positions = {(start_x, start_y)}
+    print("found start at {}".format(start_pos))
+    print("found end at {}".format(end_pos))
+    all_start_positions: Set[Point] = {start_pos}
     for y in range(len(matrix)):
         for x in range(len(matrix[0])):
             if (matrix[y][x] == 0):
-                all_start_positions.add((x, y))
+                all_start_positions.add(Point(x, y))
 
     results = []
     result1 = -1
-    for start_pos in all_start_positions:
-        path = findPath(matrix, start_pos, (end_x, end_y))
+    for start in all_start_positions:
+        path = findPath(matrix, start, end_pos)
         if not path:
             continue
         result = len(path) - 1
-        if (start_pos == (start_x, start_y)):
+        if (start == start_pos):
             result1 = result
-        results.append((start_pos, len(path) - 1))
+        results.append((start, len(path) - 1))
 
-    result2 = min([start_pos[1] for start_pos in results])
+    result2 = min([start[1] for start in results])
 
     (expected_result1, expected_result2) = expected_result.split(":")
     print("result1: {}, expected1:{}".format(result1, expected_result1))
